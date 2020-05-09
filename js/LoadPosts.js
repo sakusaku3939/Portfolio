f04feb8a5cdd00a43b06cf023507d5af4906c6de
AokiYuki <yuukiyuuki223@gmail.com>
2020-05-10 06:12:24 +0900

    css追加処理の外部化

--- js/LoadPosts.js
+++ js/LoadPosts.js
@@ -2,11 +2,11 @@ document.addEventListener("DOMContentLoaded", onLoad);
 window.addEventListener('popstate', toggle, false)
 
 //記事カードのクリックリスナー
-$('#iframe_content').load(function(){
-    const iframe = $('#iframe_content').contents();
-    iframe.find(".card").click(function(){
+$('#iframe_content').load(function () {
+    const iframe = $('#iframe_content').contents()
+    iframe.find(".card").click(function () {
         toggle()
-        history.pushState(null, null, "?p=test");
+        history.pushState(null, null, "?p=test")
     });
 });
 
@@ -16,31 +16,35 @@ function onLoad() {
 
 function toggle(isToggle = true) {
     const element = document.querySelector("#hide")
-    if (isToggle) {
-        element.classList.toggle('is-hide');
-    }
+    const posts = document.getElementById("posts")
+    const iframe = document.getElementById("iframe-posts")
+    const index = document.getElementById("index")
 
+    if (isToggle) element.classList.toggle('is-hide')
     if (element.classList.contains('is-hide')) {
-        const iframe = document.createElement("iframe")
-        iframe.setAttribute("id", "posts")
-        iframe.setAttribute("style", "" +
-            "background-color: #FAFAFA;" +
-            "    border: none;" +
-            "    position: absolute;" +
-            "    top: 55%;" +
-            "    left: 50%;" +
-            "    transform: translate(-50%, -50%);" +
-            "    -webkit-transform: translate(-50%, -50%);" +
-            "    -ms-transform: translate(-50%, -50%);" +
-            "    width: 50vw;" +
-            "    height: 100vh;"
-        )
-        iframe.setAttribute("src", "posts/test.html")
-        document.body.appendChild(iframe);
+        posts.style.display = "inline"
+        iframe.src = "posts/test.html"
+        index.style.overflowY = "scroll"
     } else {
-        const iframe = document.getElementById("posts");
-        if (iframe != null) {
-            iframe.remove()
-        }
+        posts.style.display = "none"
+        index.style.overflowY = "hidden"
     }
+}
+
+function adjust_frame_css() {
+    if (document.getElementById("iframe-posts")) {
+        const myF = document.getElementById("iframe-posts");
+        const myC = myF.contentWindow.document.documentElement;
+        let myH;
+        if (document) {
+            myH = myC.scrollHeight;
+        } else {
+            myH = myC.offsetHeight;
+        }
+        myF.style.height = myH + "px";
+    }
+}
+
+function back() {
+    window.history.back()
 }
