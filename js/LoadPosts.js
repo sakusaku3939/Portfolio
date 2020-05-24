1868e5a13119c8da0b4516a0a55eb0d9f0993173
AokiYuki <yuukiyuuki223@gmail.com>
2020-05-24 11:50:34 +0900

    レスポンシブに完全対応

--- js/LoadPosts.js
+++ js/LoadPosts.js
@@ -1,19 +1,42 @@
 document.addEventListener("DOMContentLoaded", onLoad);
 window.addEventListener('popstate', toggle, false)
 
-//記事カードのクリックリスナー
+//ウィンドウの横幅変更後リスナー
+let resizeTimer;
+let lastInnerWidth = window.innerWidth;
+window.addEventListener('resize', function (event) {
+    if (lastInnerWidth !== window.innerWidth) {
+        lastInnerWidth = window.innerWidth;
+        if (!resizeTimer) {
+            clearTimeout(resizeTimer);
+        }
+        resizeTimer = setTimeout(function () {
+            scroll_toggle()
+        }, 300);
+    }
+});
+
+//記事一覧のクリックリスナー
 $('#iframe_content').load(function () {
     const iframe = $('#iframe_content').contents()
     iframe.find(".card").click(function () {
         toggle()
         history.pushState(null, null, "?posts=test")
     });
+    iframe_height()
 });
 
+//読み込まれた場合
 function onLoad() {
     toggle(location.search === '?posts=test')
 }
 
+//戻るボタンが押された場合
+function back() {
+    window.history.back()
+}
+
+//記事表示・非表示の切り替え
 function toggle(isToggle = true) {
     const element = document.querySelector("#hide")
     const posts = document.getElementById("posts")
@@ -27,11 +50,12 @@ function toggle(isToggle = true) {
         index.style.overflowY = "scroll"
     } else {
         posts.style.display = "none"
-        index.style.overflowY = "hidden"
+        scroll_toggle()
     }
 }
 
-function adjust_frame_css() {
+//記事ページのheightを設定
+function posts_height() {
     if (document.getElementById("iframe-posts")) {
         const myF = document.getElementById("iframe-posts");
         const myC = myF.contentWindow.document.documentElement;
@@ -45,6 +69,23 @@ function adjust_frame_css() {
     }
 }
 
-function back() {
-    window.history.back()
+//スクロール表示・非表示の切り替え
+function scroll_toggle() {
+    const iframe = document.getElementById("iframe_content")
+    const index = document.getElementById("index")
+    if (window.matchMedia('(max-width: 1000px)').matches) {
+        index.style.overflowY = "scroll"
+        iframe_height()
+    } else {
+        index.style.overflowY = "hidden"
+        iframe.style.height = "100vh"
+    }
+}
+
+//記事一覧のheightを設定
+function iframe_height() {
+    const elm = document.getElementById("iframe_content");
+    if (window.matchMedia('(max-width: 1000px)').matches) {
+        elm.style.height = 30 + elm.contentWindow.document.body.scrollHeight + "px";
+    }
 }
