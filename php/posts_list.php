9f285966ddb9653acbb6d0f799b2f94d95c53e52
AokiYuki <yuukiyuuki223@gmail.com>
2020-06-08 13:31:05 +0900

    タイトル名を動的に指定する処理追加

--- php/posts_list.php
+++ php/posts_list.php
@@ -1,9 +1,49 @@
+<?php
+
+$post_data = [];
+foreach (filename('../posts/') as $folder) {
+    if ($folder !== 'style.css') {
+        $parameter = _parameter($folder);
+        $title = _title('../posts/'.$folder.'/'.$parameter.'.html');
+        $post_data[] = ['title' => $title, 'parameter' => $parameter];
+    }
+}
+
+function filename($directory) {
+    $file_list = [];
+    foreach (scandir($directory) as $name) {
+        if ($name !== '.' and $name !== '..') {
+            $file_list[] = $name;
+        }
+    }
+    return $file_list;
+}
+
+function _title($path) {
+    $content = file_get_contents($path);
+    if ($content and preg_match('!<h1>(.*?)</h1>!s', $content, $title)){
+        return $title[1];
+    }
+    return 'no title';
+}
+
+function _parameter($folder) {
+    foreach (filename('../posts/' . $folder) as $name) {
+        if (strpos($name, '.html')) {
+            return preg_replace("/(.+)(\.[^.]+$)/", "$1", $name);
+        }
+    }
+    return 'no-parameter';
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/style.css" type="text/css">
-    <link rel="stylesheet" href="css/content.css" type="text/css">
+    <link rel="stylesheet" href="../css/style.css" type="text/css">
+    <link rel="stylesheet" href="../css/posts_list.css" type="text/css">
     <!-- jQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <!-- フォント -->
@@ -15,9 +55,10 @@
     <div class="card_imgframe"></div>
     <div class="card_textbox">
         <div class="card_titletext">
-            タイトル1
+            <?php echo $post_data[1]['title']; ?>
         </div>
         <div class="card_overviewtext">
+            <?php ?>
             ここは概要となります。ここは概要となります。ここは概要となります。
         </div>
     </div>
