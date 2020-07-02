583f69fe79d8aeb597149388b5c8f2b64f2cc712
AokiYuki <yuukiyuuki223@gmail.com>
2020-07-03 03:20:13 +0900

    Twitterカードテスト

--- php/posts_list.php
+++ php/posts_list.php
@@ -95,6 +95,7 @@ function _parameter($folder)
             window.parent.sessionStorage.setItem("src", "posts/" + date + "/" + parameter + ".html")
             window.parent.history.pushState(null, null, "?posts=" + parameter)
             window.parent.posts_before_loading()
+            window.parent.setPost_data(<?php echo $json?>)
         }
     }
 </script>
