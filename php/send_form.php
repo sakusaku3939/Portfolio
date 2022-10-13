3089e217605a5b921e464e7138d861ca9785565e
Aokiti <yuukiyuuki223@gmail.com>
2022-10-14 06:06:50 +0900

    不要になった設定ファイルを削除

--- php/send_form.php
+++ php/send_form.php
@@ -1,6 +1,5 @@
 <?php
 require_once dirname(__FILE__) . '/../vendor/autoload.php';
-require_once dirname(__FILE__) . '/setting.php';
 require_once dirname(__FILE__) . '/session.php';
 
 $token = filter_input(INPUT_POST, 'token');
